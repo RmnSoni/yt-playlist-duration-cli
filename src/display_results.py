ef5534b1e3ae7554b94ae3598c0e48241d5bc465
gasher <69788442+RmnSoni@users.noreply.github.com>
2023-10-31 16:58:11 +0530

    formatted the output

--- src/display_results.py
+++ src/display_results.py
@@ -4,15 +4,17 @@ def stringify_time(seconds):
     hours, minutes = divmod(minutes,60)
     
     output_string=""
-    output_string+= str(hours) +" hours\t" if hours else "\t"
-    output_string+= str(minutes) + " minutes\t" if minutes else "\t"  
+    output_string+= str(hours) +" hours, \t" if hours else "\t"
+    output_string+= str(minutes) + " minutes, \t" if minutes else "\t"  
     output_string+= str(seconds) + " seconds"  if seconds else ""       
     
     return output_string
 
 def display_times(time_in_seconds,number_of_videos):
     speed_factors=[1, 1.25, 1.5, 1.75,2]
-    print("Total number of videos: ",number_of_videos)
-    print("Average Viedo Duration: ", stringify_time(time_in_seconds//number_of_videos))
+    print()
+    print("Total number of videos:",number_of_videos)
+    print("Average Video Duration:", stringify_time(time_in_seconds//number_of_videos))
+    print()
     for speed in speed_factors:
-        print( f'At {speed:.2f}x speed: {stringify_time(time_in_seconds/speed)}' )
+        print( f'At {speed}x speed\t: {stringify_time(time_in_seconds/speed)}' )
